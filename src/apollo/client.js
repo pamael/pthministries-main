a33d7322703a62ffade4d0eeaaa9cf548af7808b
Patrice <patrice.elias@gmail.com>
2020-01-11 23:35:08 +0000

    updated readme file

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  //uri: '/content/graphql',
-  uri: 'http://pth.xampp.local/content/graphql',
+  uri: '/content/graphql',
+  //uri: 'http://pth.xampp.local/content/graphql',
   fetch
 });
